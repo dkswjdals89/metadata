d824137c5bc8f61411240c610525b69c6dd11004
soju6jan <cybersol@naver.com>
2020-12-26 16:56:15 +0900

    release

---
+++ logic_av_censored.py
@@ -0,0 +1,216 @@
+# -*- coding: utf-8 -*-
+#########################################################
+# python
+import os, sys, traceback, re, json, threading, time, shutil
+from datetime import datetime
+# third-party
+import requests
+# third-party
+from flask import request, render_template, jsonify, redirect
+from sqlalchemy import or_, and_, func, not_, desc
+import lxml.html
+from lxml import etree as ET
+
+# sjva 공용
+from framework import db, scheduler, path_data, socketio, SystemModelSetting, app
+from framework.util import Util
+from framework.common.util import headers, get_json_with_auth_session
+from framework.common.plugin import LogicModuleBase, default_route_socketio
+# 패키지
+from .plugin import P
+logger = P.logger
+package_name = P.package_name
+ModelSetting = P.ModelSetting
+#########################################################
+
+class LogicAVCensored(LogicModuleBase, ):
+    db_default = {
+        'db_version' : '1',
+        'av_censored_use_sjva' : 'False',
+        'av_censored_order' : 'dmm, javbus',
+        'av_censored_plex_title_format' : '[{title}] {tagline}',
+        'av_censored_plex_is_proxy_preview' : 'True',
+        'av_censored_plex_landscape_to_art' : 'True',
+        'av_censored_plex_art_count' : '0',
+        'av_censored_actor_order' : 'avdbs, hentaku',
+        'av_censored_avdbs_use_proxy' : 'False',
+        'av_censored_avdbs_proxy_url' : '',
+        'av_censored_plex_manual_mode' : 'True',
+
+        'av_javbus_code' : 'ssni-900',
+        'av_javbus_use_proxy' : 'False',
+        'av_javbus_proxy_url' : '',
+        'av_javbus_image_mode' : '0',
+
+        'av_dmm_code' : 'ssni-900',
+        'av_dmm_use_proxy' : 'False',
+        'av_dmm_proxy_url' : '',
+        'av_dmm_image_mode' : '0',
+    }
+
+    def __init__(self, P):
+        super(LogicAVCensored, self).__init__(P, 'setting')
+        self.name = 'av_censored'
+
+    def process_menu(self, sub, req):
+        arg = P.ModelSetting.to_dict()
+        arg['sub'] = self.name
+        #if sub in ['setting']:
+        return render_template('{package_name}_{module_name}_{sub}.html'.format(package_name=P.package_name, module_name=self.name, sub=sub), arg=arg)
+        return render_template('sample.html', title='%s - %s' % (P.package_name, sub))
+
+    def process_ajax(self, sub, req):
+        try:
+            if sub == 'test':
+                code = req.form['code']
+                call = req.form['call']
+                if call == 'javbus':
+                    from lib_metadata.site_javbus import SiteJavbus
+                    ModelSetting.set('av_javbus_code', code)
+                    ret = {}
+                    ret['search'] = SiteJavbus.search(code, proxy_url=ModelSetting.get('av_javbus_proxy_url') if ModelSetting.get_bool('av_javbus_use_proxy') else None, image_mode=ModelSetting.get('av_javbus_image_mode'))
+                    if ret['search']['ret'] == 'success':
+                        if len(ret['search']['ret']) > 0:
+                            ret['info'] = self.info(ret['search']['data'][0]['code'])
+                elif call == 'dmm':
+                    from lib_metadata.site_dmm import SiteDmm
+                    ModelSetting.set('av_dmm_code', code)
+                    ret = {}
+                    ret['search'] = SiteDmm.search(code, proxy_url=ModelSetting.get('av_dmm_proxy_url') if ModelSetting.get_bool('av_dmm_use_proxy') else None, image_mode=ModelSetting.get('av_dmm_image_mode'))
+                    if ret['search']['ret'] == 'success':
+                        if len(ret['search']['ret']) > 0:
+                            ret['info'] = self.info(ret['search']['data'][0]['code'])
+                return jsonify(ret)
+        except Exception as e: 
+            P.logger.error('Exception:%s', e)
+            P.logger.error(traceback.format_exc())
+            return jsonify({'ret':'exception', 'log':str(e)})
+
+    def process_api(self, sub, req):
+        if sub == 'search':
+            return jsonify(self.search(req.args.get('keyword'), bool(req.args.get('manual'))))
+        elif sub == 'info':
+            return jsonify(self.info(req.args.get('code')))
+            
+    #########################################################
+
+
+    def search(self, keyword, manual):
+        ret = []
+        logger.debug(manual)
+        logger.debug(type(manual))
+        site_list = ModelSetting.get_list('av_censored_order', ',')
+        logger.debug(site_list)
+        for idx, site in enumerate(site_list):
+            if site == 'javbus':
+                from lib_metadata.site_javbus import SiteJavbus
+                data = SiteJavbus.search(
+                    keyword, 
+                    proxy_url=ModelSetting.get('av_javbus_proxy_url') if ModelSetting.get_bool('av_javbus_use_proxy') else None, 
+                    image_mode=ModelSetting.get('av_javbus_image_mode'))
+                if data['ret'] == 'success':
+                    if idx != 0:
+                        for item in data['data']:
+                            item['score'] += -1
+                    ret += data['data']
+                    ret = sorted(ret, key=lambda k: k['score'], reverse=True)  
+            elif site == 'dmm':
+                from lib_metadata.site_dmm import SiteDmm
+                data = SiteDmm.search(
+                    keyword, 
+                    proxy_url=ModelSetting.get('av_dmm_proxy_url') if ModelSetting.get_bool('av_dmm_use_proxy') else None, 
+                    image_mode=ModelSetting.get('av_dmm_image_mode'))
+                if data['ret'] == 'success':
+                    if idx != 0:
+                        for item in data['data']:
+                            item['score'] += -1
+                    ret += data['data']
+                    ret = sorted(ret, key=lambda k: k['score'], reverse=True)  
+            if manual:
+                if ModelSetting.get_bool('av_censored_plex_manual_mode'):
+                    continue
+                else:
+                    if len(ret) > 0 and ret[0]['score'] == 100:
+                        break
+            else:
+                if len(ret) > 0 and ret[0]['score'] == 100:
+                    break
+        return ret
+    
+
+    def info(self, code):
+        ret = None
+        if ModelSetting.get_bool('av_censored_use_sjva'):
+            ret = P.get_metadata(code)
+        if ret is None:
+            if code[1] == 'B':
+                from lib_metadata.site_javbus import SiteJavbus
+                ret = self.info2(code, SiteJavbus, 'javbus')
+            elif code[1] == 'D':
+                from lib_metadata.site_dmm import SiteDmm
+                ret = self.info2(code, SiteDmm, 'dmm')
+        
+        if ret is not None:
+            ret['plex_is_proxy_preview'] = ModelSetting.get_bool('av_censored_plex_is_proxy_preview')
+            ret['plex_is_landscape_to_art'] = ModelSetting.get_bool('av_censored_plex_landscape_to_art')
+            ret['plex_art_count'] = ModelSetting.get_int('av_censored_plex_art_count')
+
+            if ret['actor'] is not None:
+                for item in ret['actor']:
+                    self.process_actor(item)
+
+            ret['title'] = ModelSetting.get('av_censored_plex_title_format').format(
+                originaltitle=ret['originaltitle'], 
+                plot=ret['plot'],
+                title=ret['title'],
+                sorttitle=ret['sorttitle'],
+                runtime=ret['runtime'],
+                country=ret['country'],
+                premiered=ret['premiered'],
+                year=ret['year'],
+                actor=ret['actor'][0]['name'] if ret['actor'] is not None and len(ret['actor']) > 0 else '',
+                tagline=ret['tagline']
+            )
+            return ret
+
+    def info2(self, code, site_class, site_name):
+        from lib_metadata.site_javbus import SiteJavbus
+        image_mode = ModelSetting.get('av_{site_name}_image_mode'.format(site_name=site_name))
+        data = site_class.info(
+            code,
+            proxy_url=ModelSetting.get('av_{site_name}_proxy_url'.format(site_name=site_name)) if ModelSetting.get_bool('av_{site_name}_use_proxy'.format(site_name=site_name)) else None, 
+            image_mode=image_mode)
+        if data['ret'] == 'success':
+            ret = data['data']
+            if ModelSetting.get_bool('av_censored_use_sjva') and image_mode == '3' and SystemModelSetting.get('trans_type') == '1' and SystemModelSetting.get('trans_google_api_key') != '' and len(ret['thumb']) == 2 and ret['thumb'][0]['value'].find('discordapp.net') != -1 and ret['thumb'][1]['value'].find('discordapp.net') != -1:
+                P.set_metadata(code, ret)
+        return ret
+
+    def process_actor(self, entity_actor):
+        actor_site_list = ModelSetting.get_list('av_censored_actor_order', ',')
+        for site in actor_site_list:
+            if site == 'hentaku':
+                from lib_metadata.site_hentaku import SiteHentaku
+                self.process_actor2(entity_actor, SiteHentaku, 'H', None)
+            elif site == 'avdbs':
+                from lib_metadata.site_avdbs import SiteAvdbs
+                self.process_actor2(entity_actor, SiteAvdbs, 'A', ModelSetting.get('av_censored_avdbs_proxy_url') if ModelSetting.get_bool('av_censored_avdbs_use_proxy') else None)
+            if entity_actor['name'] is not None:
+                return
+        if entity_actor['name'] is None:
+            entity_actor['name'] = entity_actor['originalname'] 
+
+
+    def process_actor2(self, entity_actor, site_class, site_char, proxy_url):
+        if ModelSetting.get_bool('av_censored_use_sjva'):
+            data = P.get_metadata('A' + site_char + entity_actor['originalname'])
+            if data is not None:
+                entity_actor['name'] = data['name']
+                entity_actor['name2'] = data['name2']
+                entity_actor['thumb'] = data['thumb']
+                return
+        from lib_metadata.site_hentaku import SiteHentaku
+        site_class.get_actor_info(entity_actor, proxy_url=proxy_url)
+        if entity_actor['name'] is not None and entity_actor['thumb'] is not None and entity_actor['thumb'].find('discordapp.net') != -1:
+            P.set_metadata('A'+ site_char + entity_actor['originalname'], entity_actor)
+            return
---
+++ plugin.py
@@ -0,0 +1,231 @@
+# -*- coding: utf-8 -*-
+# python
+import os, traceback, time, json
+
+# third-party
+import requests
+from flask import Blueprint, request, send_file, redirect
+
+# sjva 공용
+from framework import app, path_data, check_api, py_urllib, SystemModelSetting
+from framework.logger import get_logger
+from framework.util import Util
+from framework.common.plugin import get_model_setting, Logic, default_route
+# 패키지
+#########################################################
+server_plugin_ddns = 'https://sjva-server.soju6jan.com'
+class P(object):
+    package_name = __name__.split('.')[0]
+    logger = get_logger(package_name)
+    blueprint = Blueprint(package_name, package_name, url_prefix='/%s' %  package_name, template_folder=os.path.join(os.path.dirname(__file__), 'templates'))
+    menu = {
+        'main' : [package_name, u'메타데이터'],
+        'sub' : [
+            ['setting', u'설정'], ['av_censored', u'AV Censored'], ['log', u'로그']
+        ], 
+        'category' : 'service',
+        'sub2' : {
+            'av_censored' : [
+                ['setting', u'기본 설정'], ['javbus', 'Javbus'], ['dmm', 'DMM'],
+            ],
+        }
+    }  
+
+    plugin_info = {
+        'version' : '0.2.0.0',
+        'name' : package_name,
+        'category_name' : 'service',
+        'icon' : '',
+        'developer' : u'soju6jan',
+        'description' : u'Metadata',
+        'home' : 'https://github.com/soju6jan/%s' % package_name,
+        'more' : '',
+    }
+    ModelSetting = get_model_setting(package_name, logger)
+    logic = None
+    module_list = None
+    home_module = 'setting'
+
+    @staticmethod
+    def get_metadata(code):
+        try:
+            from framework import py_urllib
+            url = '{server_plugin_ddns}/server/normal/metadata/get?code={code}'.format(server_plugin_ddns=server_plugin_ddns, code=code)
+            data = requests.get(url).json()
+            if data['ret'] == 'success':
+                return data['data']
+        except Exception as exception: 
+            logger.error('Exception:%s', exception)
+            logger.error(traceback.format_exc()) 
+
+    @staticmethod
+    def set_metadata(code, data):
+        try:
+            from framework import py_urllib
+            url = '{server_plugin_ddns}/server/normal/metadata/set'.format(server_plugin_ddns=server_plugin_ddns)
+            param = {'code':code, 'data':json.dumps(data), 'user':SystemModelSetting.get('sjva_me_user_id')}
+            data = requests.post(url, data=param).json()
+            if data['ret'] == 'success':
+                logger.info('%s Data save success. Thanks!!!!', code)
+        except Exception as exception: 
+            logger.error('Exception:%s', exception)
+            logger.error(traceback.format_exc()) 
+
+
+
+def initialize():
+    try:
+        app.config['SQLALCHEMY_BINDS'][P.package_name] = 'sqlite:///%s' % (os.path.join(path_data, 'db', '{package_name}.db'.format(package_name=P.package_name)))
+        from framework.util import Util
+        Util.save_from_dict_to_json(P.plugin_info, os.path.join(os.path.dirname(__file__), 'info.json'))
+
+        from .logic_av_censored import LogicAVCensored
+        P.module_list = [LogicAVCensored(P)]
+        P.logic = Logic(P)
+        default_route(P)
+    except Exception as e: 
+        P.logger.error('Exception:%s', e)
+        P.logger.error(traceback.format_exc())
+
+logger = P.logger
+
+initialize()
+
+
+
+
+
+#########################################################
+# API - 외부
+#########################################################
+@P.blueprint.route('/api/<sub>', methods=['GET', 'POST'])
+@check_api
+def baseapi(sub):
+    try:
+        if sub == 'image':
+            from PIL import Image
+            # 2020-06-02 proxy 사용시 포스터처리
+            image_url = request.args.get('url')
+            logger.debug(image_url)
+            method = ModelSetting.get('javdb_landscape_poster')
+            if method == '0':
+                if FileProcess.Vars.proxies is None:
+                    return redirect(image_url)
+                else:
+                    im = Image.open(requests.get(image_url, stream=True, proxies=FileProcess.Vars.proxies).raw)
+                    filename = os.path.join(path_data, 'tmp', 'rotate.jpg')
+                    im.save(filename)
+                    return send_file(filename, mimetype='image/jpeg')
+            
+            im = Image.open(requests.get(image_url, stream=True, proxies=FileProcess.Vars.proxies).raw)
+            width,height = im.size
+            logger.debug(width)
+            logger.debug(height)
+            if height > width * 1.5:
+                return redirect(image_url)
+            if method == '1':
+                if width > height:
+                    im = im.rotate(-90, expand=True)
+            elif method == '2':
+                if width > height:
+                    im = im.rotate(90, expand=True)
+            elif method == '3':
+                new_height = int(width * 1.5)
+                new_im = Image.new('RGB', (width, new_height))
+                new_im.paste(im, (0, int((new_height-height)/2)))
+                im = new_im
+
+            filename = os.path.join(path_data, 'tmp', 'rotate.jpg')
+            im.save(filename)
+            return send_file(filename, mimetype='image/jpeg')
+
+        elif sub == 'image_proxy':
+            from PIL import Image
+            image_url = py_urllib.unquote_plus(request.args.get('url'))
+            proxy_url = request.args.get('proxy_url')
+            if proxy_url is not None:
+                proxy_url = py_urllib.unquote_plus()
+
+            logger.debug('image_url : %s', image_url)
+            #2020-09-21 핸드쉐이크 에러
+            from system.logic_command import SystemLogicCommand
+            filename = os.path.join(path_data, 'tmp', 'proxy_%s.jpg' % str(time.time()) )
+
+            #im = Image.open(requests.get(image_url, stream=True, verify=False, proxies=FileProcess.Vars.proxies).raw)
+            #im.save(filename)
+            if proxy_url is not None and proxy_url != '':
+                # 알파인 도커 wget 에 -e 옵션 안먹음
+                #tmp = image_url.split('//')
+                #if len(tmp) == 2:
+                #    image_url = tmp[1]
+                #command = ['wget', '-O', filename, image_url, '-e', 'use_proxy=yes', '-e', 'http_proxy=%s' % ModelSetting.get('proxy_url').replace('https://', '').replace('http://', '')]
+                #command = ['curl', '-o', filename, image_url, '-x', proxy_url.replace('https://', '').replace('http://', '')]
+                command = ['curl', '-o', filename, image_url, '-x', proxy_url]
+                logger.debug(' '.join(command))
+                ret = SystemLogicCommand.execute_command_return(command)
+            else:
+                #tmp = image_url.split('//')
+                #if len(tmp) == 2:
+                #    image_url = tmp[1]
+                ret = SystemLogicCommand.execute_command_return(['curl', '-o', filename, image_url])
+            
+            return send_file(filename, mimetype='image/jpeg')
+        elif sub == 'discord_proxy':
+            from framework.common.notify import discord_proxy_image
+            image_url = py_urllib.unquote_plus(request.args.get('url'))
+            ret = discord_proxy_image(image_url)
+            #logger.debug(ret)
+            return redirect(ret)
+            from PIL import Image
+            
+            im = Image.open(requests.get(ret, stream=True, verify=False).raw)
+            filename = os.path.join(path_data, 'tmp', 'proxy.jpg')
+            im.save(filename)
+            return send_file(filename, mimetype='image/jpeg')
+        """
+        elif sub == 'image_process':
+            mode = request.args.get('mode')
+            if mode == 'landscape_to_poster':
+                from PIL import Image
+                image_url = py_urllib.unquote_plus(request.args.get('url'))
+                im = Image.open(requests.get(image_url, stream=True).raw)
+                width, height = im.size
+                left = width/1.895734597
+                top = 0
+                right = width
+                bottom = height
+                filename = os.path.join(path_data, 'tmp', 'proxy_%s.jpg' % str(time.time()) )
+                poster = im.crop((left, top, right, bottom))
+                poster.save(filename)
+                return send_file(filename, mimetype='image/jpeg')
+        """
+
+    except Exception as e:
+        logger.debug('Exception:%s', e)
+        logger.debug(traceback.format_exc())
+
+
+@P.blueprint.route('/normal/<sub>', methods=['GET', 'POST'])
+def basenormal(sub):
+    try:
+        if sub == 'image_process.jpg':
+            mode = request.args.get('mode')
+            if mode == 'landscape_to_poster':
+                from PIL import Image
+                image_url = py_urllib.unquote_plus(request.args.get('url'))
+                im = Image.open(requests.get(image_url, stream=True).raw)
+                width, height = im.size
+                left = width/1.895734597
+                top = 0
+                right = width
+                bottom = height
+                filename = os.path.join(path_data, 'tmp', 'proxy_%s.jpg' % str(time.time()) )
+                poster = im.crop((left, top, right, bottom))
+                poster.save(filename)
+                return send_file(filename, mimetype='image/jpeg')
+    except Exception as e:
+        logger.debug('Exception:%s', e)
+        logger.debug(traceback.format_exc())
+
+
+
