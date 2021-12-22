6642682f49e0448583841cfacd0da7a3d77997df
105PM <105pm.dev@gmail.com>
2021-12-22 17:22:02 +0900

    add heyzo

--- logic_jav_uncensored.py
+++ logic_jav_uncensored.py
@@ -13,7 +13,7 @@ from tool_base import ToolUtil, ToolBaseNotify
 # lib_metadata
 from lib_metadata.server_util import MetadataServerUtil
 # import sites
-from lib_metadata import SiteUtil, Site1PondoTv, Site10Musume
+from lib_metadata import SiteUtil, Site1PondoTv, Site10Musume, SiteHeyzo
 
 #########################################################
 from .plugin import P
@@ -35,10 +35,13 @@ class LogicJavUncensored(LogicModuleBase):
         f'{module_name}_10musume_use_proxy' : 'False',
         f'{module_name}_10musume_proxy_url' : '',
         f'{module_name}_10musume_code' : '010620_01',
+        f'{module_name}_heyzo_use_proxy' : 'False',
+        f'{module_name}_heyzo_proxy_url' : '',
+        f'{module_name}_heyzo_code' : 'HEYZO-2681',
 
     }
 
-    module_map = {'1pondo': Site1PondoTv, '10musume': Site10Musume}
+    module_map = {'1pondo': Site1PondoTv, '10musume': Site10Musume, 'heyzo': SiteHeyzo}
 
     def __init__(self, P):
         super(LogicJavUncensored, self).__init__(P, 'setting')
@@ -132,6 +135,18 @@ class LogicJavUncensored(LogicModuleBase):
 
             if data['ret'] == 'success' and len(data['data']) > 0:
                 ret += data['data']
+
+        elif 'heyzo' in keyword.lower():
+            from lib_metadata.site_uncensored.site_heyzo import SiteHeyzo as SiteClass
+
+            data = SiteClass.search(
+                keyword, 
+                do_trans=do_trans,
+                proxy_url=ModelSetting.get('jav_uncensored_{site_name}_proxy_url'.format(site_name=SiteClass.site_name)) if ModelSetting.get_bool('jav_uncensored_{site_name}_use_proxy'.format(site_name=SiteClass.site_name)) else None, 
+                image_mode='3',manual=manual)
+
+            if data['ret'] == 'success' and len(data['data']) > 0:
+                ret += data['data']
                 
 
         else:
@@ -140,6 +155,8 @@ class LogicJavUncensored(LogicModuleBase):
                     from lib_metadata.site_uncensored.site_1pondotv import Site1PondoTv as SiteClass
                 elif site == '10musume':
                     from lib_metadata.site_uncensored.site_10musume import Site10Musume as SiteClass
+                elif site == 'heyzo':
+                    from lib_metadata.site_uncensored.site_Heyzo import SiteHeyzo as SiteClass
 
                 data = SiteClass.search(
                 keyword, 
@@ -175,6 +192,9 @@ class LogicJavUncensored(LogicModuleBase):
             elif code[1] == 'M':
                 from lib_metadata import Site10Musume
                 ret = self.info2(code, Site10Musume)
+            elif code[1] == 'H':
+                from lib_metadata import SiteHeyzo
+                ret = self.info2(code, SiteHeyzo)
             
         
         if ret is not None:
